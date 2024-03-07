82c1430a412242da40b751f6150d167e9984e692
DominikBoeck <BOE230089@spengergasse.at>
2024-03-07 13:42:17 +0100

    Commit

--- 2024-02-29/script.js
+++ 2024-02-29/script.js
@@ -3,8 +3,21 @@ const { fakerDE } = require('@faker-js/faker');
 for (let i = 0; i < 10; i++) {
     console.log(fakerDE.airline.airplane().name);
 }
+
 for (let i = 0; i < 10; i++) {
     console.log(fakerDE.finance.iban());
 }
 
+for (let i = 0; i < 10; i++) {
+    console.log(fakerDE.hacker.verb());
+}
 
+for (let i = 0; i < 10; i++) {
+    console.log(fakerDE.person.lastName());
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(fakerDE.animal.dog());
+}
+
+console.log("end")
